e79fb184436e81296cd475774681f981544bc92d
fei.liang <liangfei2016x@163.com>
2017-07-05 16:29:51 +0800

    处理中文,生成指定图片样式

--- Demo2.py
+++ Demo2.py
@@ -1,8 +1,11 @@
 #encoding='utf-8'
-from wordcloud import WordCloud
+from wordcloud import WordCloud,ImageColorGenerator
 import matplotlib.pyplot as plt
 import os
-from os import path
+import jieba
+from scipy.misc import imread
+import numpy as np
+from PIL import Image
 
 
 def readTxt(data):
@@ -10,14 +13,29 @@ def readTxt(data):
         line=f.read()
         return line
 
+def chinese(text):
+    #text="故宫的著名景点包括乾清宫、太和殿和午门等。其中乾清宫非常精美，午门是紫禁城的"
+    segs = jieba.cut(text)
+    return  " ".join(segs)
+    # for seg in segs:
+    #     print(seg)
+
 def showImg(txt):
     font = r'C:\Windows\Fonts\simfang.ttf' #设置中文
-    wc = WordCloud(font_path=font).generate(txt)
+    imgPath = imread(os.getcwd()+'/ali.jpg')
+    #imgPath = np.array(Image.open(os.getcwd()+'/ali.jpg'))
+    wc = WordCloud(background_color="white", font_path=font, mask=imgPath, max_font_size=150)
+    wc.generate(txt)
+    #recolor 把填充的字体设置为图片本身的颜色 wc.recolor(color_func=ImageColorGenerator(imgPath)
     plt.imshow(wc,interpolation='bilinear')
     plt.axis("off")
     plt.show()
+    #保存图片
+    wc.to_file(os.getcwd()+"/newAli.jpg")
 
 
 if __name__ == '__main__':
        t = readTxt(os.getcwd()+'/test.txt')
-       showImg(t)
+       ykzzldx = chinese(t)
+       showImg(ykzzldx)
+
